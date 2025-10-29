1a13a8fa624df90fadfb8620abe0c9bc4493b852
SIKKTIN <2071053497@qq.com>
2025-10-29 23:31:20 +0800

    Initial commit

---
+++ src/main/java/com/sikktin/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.sikktin.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
